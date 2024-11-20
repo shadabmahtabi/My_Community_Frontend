fb4be89e4cf8625eacc921f714b0775f7f4dbe83
Shaaz <asadakhtar800@gmail.com>
2024-11-20 16:54:35 +0530

    Add Admin Login Form

--- src/components/Auth/AdminLogin.jsx
+++ src/components/Auth/AdminLogin.jsx
@@ -3,15 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../UI/Navbar';
 import Footer from '../UI/Footer';
+import LoginForm from '../adminComponent/LoginForm';
 
 const AdminLogin = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
+  const handleLogin = async (username, password) => {
     try {
       const response = await axios.post('http://localhost:3000/api/users/admin-login', { username, password });
       const { token } = response.data;
@@ -35,43 +32,14 @@ const AdminLogin = () => {
 
   return (
     <>
-    <Navbar/>
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
-        <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">Admin Login</h2>
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700 font-medium mb-2">Username</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 font-medium mb-2">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200"
-          >
-            Login
-          </button>
-        </form>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+          <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">Admin Login</h2>
+          <LoginForm onLogin={handleLogin} />
+        </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
